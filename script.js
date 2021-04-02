54eeed8c1b00ca060d5f77df622cd01caeba2420
Serena Chandler <serenachandler2019@gmail.com>
2021-04-01 17:35:19 -0700

    added a button for 9am

--- script.js
+++ script.js
@@ -1,10 +1,15 @@
 
-
-
+var timeBlocks = $("timeBlocks");
+var saveButton = $("<button>");
+var dailyGoals = $("<input>");
+var timeOfDay = $("<h1>");
+var container = $("container");
+var time = ["9", "10", "11", "12", "1", "2", "3", "4", "5",]
+var currentHour = moment().format("H")
 let updateTime = function () {
-    let currentTime = moment().format('MMM: Do: dddd: h:mm:s')
+    let currentTime = moment().format('MMMM Do, dddd: h:mm:s')
     $("#currentDay").text(currentTime)
-}
+    }
 
 
     updateTime();
@@ -13,3 +18,14 @@ let updateTime = function () {
 
     setInterval(updateTime, 1000);
 
+
+timeOfDay.text(time[0] + "AM")
+saveButton.text("SAVE");
+dailyGoals.append(saveButton);
+timeBlocks.append(timeOfDay);
+container.append(timeBlocks);
+
+console.log(currentHour)
+
+
+// var target = event.target.getAttribute("data-index");
